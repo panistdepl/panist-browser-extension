635ae92be8178afc769df75141e2a12f08c48062
bouri <mounir.bouri@esprit.tn>
2018-11-22 10:19:34 +0100

    changer le chemin de fichier liste établissements .json

--- src_firefox/options/options.js
+++ src_firefox/options/options.js
@@ -99,7 +99,7 @@ $(document).ready(function () {
   });
 
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://bouri-mounir.github.io/ipetablist.json?date=" + Date.now(), true);
+  xhr.open("GET", "https://panistdepl.github.io/panist-browser-extension/19.11.18_listeInstitutions.json?date=" + Date.now(), true);
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
       var data = JSON.parse(xhr.responseText)
